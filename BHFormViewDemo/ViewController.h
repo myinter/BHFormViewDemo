533241a285da045a7b643a1fedaf2872b6527ff0
熊伟 <xiongwei@macbookpro-6ccd.ztgame.com>
2016-05-16 17:18:09 +0800

    Initial Commit

---
+++ BHFormViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BHFormViewDemo
+//
+//  Created by 熊伟 on 2016/5/16.
+//  Copyright © 2016年 熊伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BHFormViewDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BHFormViewDemo
+//
+//  Created by 熊伟 on 2016/5/16.
+//  Copyright © 2016年 熊伟. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
