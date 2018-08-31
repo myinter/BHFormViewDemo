2a829887f1bc40498dc1897194c7034856a31990
熊韦华 <bighiung@bighiungde-macbook-pro.local>
2018-08-31 12:30:22 +0800

    修正bug。

--- BHFormViewDemo/BHFormView/BHFormView.m
+++ BHFormViewDemo/BHFormView/BHFormView.m
@@ -226,7 +226,7 @@ dispatch_queue_t SerialQueue = nil;
 	while (1) {
 		if (upperIndex != rowCount) {
 			BHFormViewRow *row = _Rows[upperIndex];
-			if (row.hasVeryHighCell || (row.beginX > visibleMaxX || row.maxX < visibleMinX) || (row.beginY > visibleMaxY || row.maxY < visibleMinY)) {
+			if ((row.beginX > visibleMaxX || row.maxX < visibleMinX) || (row.beginY > visibleMaxY || row.maxY < visibleMinY)) {
 				row.visible = NO;
 				if (hasUpperRowVisible) {
 					for (NSInteger rc = upperIndex; rc != rowCount; rc++) {
@@ -285,7 +285,7 @@ dispatch_queue_t SerialQueue = nil;
 		
 		if (lowerIndex != -1) {
 			BHFormViewRow *row = _Rows[lowerIndex];
-			if (row.hasVeryHighCell || (row.beginX > visibleMaxX || row.maxX < visibleMinX) || (row.beginY > visibleMaxY || row.maxY < visibleMinY)) {
+			if ((row.beginX > visibleMaxX || row.maxX < visibleMinX) || (row.beginY > visibleMaxY || row.maxY < visibleMinY)) {
 				row.visible = NO;
 				if (hasLowerRowVisible) {
 					for (NSInteger rc = lowerIndex; rc >= 0; rc--) {
@@ -350,7 +350,7 @@ dispatch_queue_t SerialQueue = nil;
 		for (BHFormViewRow *row in _Rows) {
 			NSInteger visibleMinIndex = NSNotFound;
 			NSInteger visibleMaxIndex = 0;
-			if (row.visible) {
+			if (row.visible || row.hasVeryHighCell) {
 				CGRect *rects = row.rectsForCells;
 				BOOL *columnVisibles = row.columnsVisible;
 				NSInteger columnCount = row.columnCount;
